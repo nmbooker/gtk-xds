8f406ed269711d4028474292d1e025e4e9937130
Nick Booker <NMBooker@gmail.com>
2012-12-22 20:47:27 +0000

    More scripts to help with Debian package patching process.

---
+++ ubuntu/patcher_config.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+"""Make sure you don't os.chdir() until AFTER you have imported this.
+"""
+
+import ConfigParser
+import os
+
+IMPORTED_WD = os.getcwd()
+
+class Config(object):
+    """Configuration for patch scripts for GTK DnD.
+    """
+    def __init__(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(os.path.join(IMPORTED_WD, 'build.ini'))
+
+    def source_package(self):
+        return self.config.get('package', 'source_package')
+
+    def patches_subdir(self):
+        return self.config.get('package', 'patches_subdir')
+
+    def patches_fullpath(self):
+        subdir = self.patches_subdir()
+        return os.path.realpath(os.path.join(IMPORTED_WD, subdir))
